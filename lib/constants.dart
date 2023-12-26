089839cad924f0ebd8851332269d9518b90c05c9
Ishan Nadeera <IT20016616@my.sliit.lk>
2023-12-26 14:49:33 +0530

    Added constants with colors

---
+++ lib/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const kBackgroundColor = Color.fromARGB(255, 255, 255, 255);
+const kPrimaryColor = Color.fromARGB(255, 0, 173, 212);
+const kSecondaryColor = Color.fromARGB(255, 1, 178, 93);
